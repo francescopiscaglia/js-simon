04a1b25e3bd0a27e01d9abae64a3fc49c895a8dd
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-10 14:15:21 +0200

    add for loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
-console.log("Hello world");
+// - Visualizzare in pagina 5 numeri casuali.
+
+// creare un'array per salvare questi numeri
+let randomNumber = [];
+
+// creare la funzione che generi i numeri casuali
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// uso il ciclo for per pushare i 5 numeri dentro all'array
+for (let i = 0; i < 5; i++) {
+
+    // richiamo la funzione e salvo i numeri nella costante number
+    const number = getRndInteger(1, 100);
+    // pusho i numeri dentro all'array
+    randomNumber.push(number);
+}
+
+console.log(randomNumber);
+
+// far partire un timer di 30 sec
+
+// far scomparire i numeri
+
+// far apparire 5 input per l'utente
+
+
+
+// verificare se i numeri inseriti dall'utente corrispondo a quelli generati
+
+
+// mostrare il risultato
